a78e1227a76d19602d8abdadd6352c47522bd918
Dario Quental <dhdq@hotmail.com>
2026-02-25 17:29:41 +0000

    Aula 1 e Exercicios e livros

---
+++ Aula1/ciclo_de_repetiçao.py
@@ -0,0 +1,23 @@
+# while 
+# for -> foreach
+# repetirem ações 
+ 
+
+#numerostring="1"
+#print(ord(numerostring))
+
+#while True :
+#    para=int(input("1 para parar?"))
+#    if para==1:
+#        break     # continue 
+    #mais codigo , fazer invasao mundial
+
+lista=[1,2,34,5,6,7,7,8]
+
+print(len(lista))
+
+for i in range(len(lista)):
+    print(lista[i])
+
+for listaelemento in lista:
+    print(listaelemento)
---
+++ Aula1/variaveis_tipo.py
@@ -0,0 +1,47 @@
+# string
+nome="Joao Carvalho"
+
+print(type(nome))
+print(nome)
+
+# int
+idade=32
+
+print(type(idade))
+print(idade)
+
+# float
+media=32.5
+
+print(type(media))
+print(media)
+
+# float
+media=32
+
+print(type(media))
+print(media)
+
+# boolean
+flag=True
+
+print(type(flag))
+print(flag)
+
+# [] <- Lista
+
+lista=[1,"Dario",3.4,False]
+#index 0  1       2    3
+print(type(lista))
+print(lista)
+# acesso valor ao 3 possição
+print(lista[2])
+
+# () <- tupple lista nao modificavel 
+
+nome="nome1"
+idade="idade1"
+tupla=(nome,idade)
+print(tupla)
+# acesso valor ao 2 possição do index
+print(tupla[1])
