5f24ad262c667135ded38acc60f6fd7bbc68d450
Dario Quental <dhdq@hotmail.com>
2026-05-06 17:20:32 +0100

    correcto

--- Aula8/testScapy.py
+++ Aula8/testScapy.py
@@ -2,6 +2,6 @@ from scapy.all import IP,ICMP,sr1,sniff
 
 print("Scapy ok")
 
-resposta = sniff(sr1(IP(dst="8.8.8.8")/ICMP(), timeout=2,verbose=0))
+resposta = sr1(IP(dst="8.8.8.8")/ICMP(), timeout=2, verbose=0)
 
 print(resposta)
